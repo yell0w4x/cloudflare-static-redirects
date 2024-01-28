69cbd3ff9378e40a711257be633c93f17443e501
yell0w4x <yellow4x@gmail.com>
2024-01-28 23:04:42 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# cloudflare-static-redirects
-Cloudflare static files serving worker with redirects support
+# Worker for Cloudflare Workers Sites that supports simple redirects
+
+Simple support for [pages redirects](https://developers.cloudflare.com/pages/configuration/redirects/).
+No wildcard or placeholders or any other stuff supported.
+
+Just put `_redirects` file into the root of a folder containg static files to be deployed and 
+put a reference to `index.ts` in you `wrangler.toml` as `main = "index.ts"`.
+
+The content of `_redirects` is as follows.
+```
+/original/path1 /new/path2 301
+/original/path2 /new/path2 301
+...
+```
---
+++ index.ts
@@ -0,0 +1,74 @@
+import { getAssetFromKV } from '@cloudflare/kv-asset-handler';
+import manifestJSON from '__STATIC_CONTENT_MANIFEST';
+const assetManifest = JSON.parse(manifestJSON);
+
+
+async function getRedirects(staticContent) {
+    const realKey = assetManifest['_redirects'];
+    const redirectStr = realKey && await staticContent.get(realKey);
+
+    let result = {};
+
+    for (let s of redirectStr.split('\n')) {
+        let values = s.split(/\s+/);
+        if (values.length < 3) {
+            continue;
+        }
+        result[values[0]] = { 'to': values[1], 'method': parseInt(values[2]) };
+    }
+
+    return result;
+}
+
+
+function makeNotFoundResponse(pathname) {
+    return new Response(`${pathname} not found`, {
+        status: 404,
+        statusText: 'Not Found'
+    });
+}
+
+
+export default {
+    async fetch(request, env, ctx) {
+        try {
+                try {
+                    const redirects = await getRedirects(env.__STATIC_CONTENT);
+                    const pathname = new URL(request.url).pathname;
+                    if (pathname in redirects) {
+                        let newUrl = new URL(redirects[pathname]['to'], new URL(request.url).origin);
+                        return Response.redirect(newUrl, redirects[pathname]['method']);
+                    }
+                } catch (e) {
+                    console.log(`Redirect failed [${e.stack}]`)
+                }
+
+            return await getAssetFromKV({
+                    request,
+                    waitUntil: ctx.waitUntil.bind(ctx),
+                }, {
+                    ASSET_NAMESPACE: env.__STATIC_CONTENT,
+                    ASSET_MANIFEST: assetManifest,
+                });
+        } catch (e) {
+            try {
+                const realKey = assetManifest['404.html'];
+                const value = realKey && await env.__STATIC_CONTENT.get(realKey);
+                if (value == null) {
+                    const pathname = new URL(request.url).pathname;
+                    return makeNotFoundResponse(pathname);
+                }
+                return new Response(value, {
+                    status: 404,
+                    statusText: 'Not Found',
+                    headers: {'content-type': 'text/html'}
+                });
+            } catch (e) {
+                return new Response(`$${e.stack}`, {
+                    status: 500,
+                    statusText: 'Internal Server Error',
+                });
+            }
+        }
+    }
+};
